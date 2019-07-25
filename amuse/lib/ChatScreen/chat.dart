d16cee00e3d9bc4782a3bcd84a15292dd21f5ef9
dhwani-chauhan <1997.dhwanic@gmail.com>
2019-07-25 16:15:26 +0530

    validationClass Testing Added

--- amuse/lib/ChatScreen/chat.dart
+++ amuse/lib/ChatScreen/chat.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:io';
 
 import 'chat_List.dart';
-import 'firebase_Connection.dart';
+import '../firebase_Connection.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
