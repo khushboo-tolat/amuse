4653bd1967a7f79e8c588502658740259cf5d449
dhwani-chauhan <1997.dhwanic@gmail.com>
2019-07-29 16:48:11 +0530

    Username Testing

--- amuse/test/validationClass_test.dart
+++ amuse/test/validationClass_test.dart
@@ -2,6 +2,50 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:amuse/Validation/validationClass.dart';
 
 void main(){
+  //Validate UserName
+  test('empty Username returns error string', () {
+
+    final result = Validation.validateUsername('');
+    expect(result, 'Username cannot be empty');
+  });
+
+  test('non-empty Username not length of 4 returns error string', () {
+
+    final result = Validation.validateUsername('Abc');
+    expect(result, 'Atleast 4 characters required');
+  });
+
+  test('non-empty Username length of 4 of characters returns error string', () {
+
+    final result = Validation.validateUsername('PINM');
+    expect(result, null);
+  });
+
+  test('non-empty Username length of 4 of characters returns error string', () {
+
+    final result = Validation.validateUsername('pinm');
+    expect(result, null);
+  });
+
+  test('non-empty Username length of 4 of characters returns error string', () {
+
+    final result = Validation.validateUsername('1234');
+    expect(result, 'Only characters are allowed');
+  });
+
+  test('non-empty Username length of 4 of characters returns error string', () {
+
+    final result = Validation.validateUsername('._=,');
+    expect(result, 'Only characters are allowed');
+  });
+
+  test('non-empty Username length of 26 of characters returns error string', () {
+
+    final result = Validation.validateUsername('qwertyuiopasdfghjklzxcvbnm');
+    expect(result, 'Not more than 25 characters required');
+  });
+  
+  //Validate PIN Number
   test('empty PIN returns error string', () {
 
     final result = Validation.validatePIN('');
